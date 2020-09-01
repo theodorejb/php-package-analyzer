e2bfa33f5495f6bb54a79517ebce68b2aafc4643
Viktor Szépe <viktor@szepe.net>
2020-09-01 03:28:45 +0200

    Proper coding in Analyzer

--- src/Analyzer.php
+++ src/Analyzer.php
@@ -4,12 +4,17 @@ declare(strict_types=1);
 
 namespace theodorejb\PackageAnalyzer;
 
+use Generator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
 class Analyzer
 {
-    public function getPhpFiles(string $path): \Generator
+    public function getPhpFiles(string $path): Generator
     {
-        $iterator = new \RecursiveIteratorIterator(
-            new \RecursiveDirectoryIterator($path), \RecursiveIteratorIterator::LEAVES_ONLY
+        $iterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator($path),
+            RecursiveIteratorIterator::LEAVES_ONLY
         );
 
         /** @var \DirectoryIterator $file */
