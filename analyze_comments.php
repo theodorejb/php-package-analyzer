39821f2ccd4d70fe297e51038bbc6eb4ca7f8271
Viktor Szépe <viktor@szepe.net>
2020-09-01 03:26:26 +0200

    Proper coding in analyze_comments.php

--- analyze_comments.php
+++ analyze_comments.php
@@ -2,24 +2,33 @@
 
 declare(strict_types=1);
 
+use PhpParser\Error as ParserError;
+use PhpParser\Lexer\Emulative;
+use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitorAbstract;
+use PhpParser\Parser\Php7;
 use theodorejb\PackageAnalyzer\Analyzer;
 
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
-$lexer = new PhpParser\Lexer\Emulative([
+$lexer = new Emulative([
     'usedAttributes' => [
-        'comments', 'startLine', 'endLine',
-        'startFilePos', 'endFilePos',
+        'comments',
+        'startLine',
+        'endLine',
+        'startFilePos',
+        'endFilePos',
     ]
 ]);
 
-$parser = new PhpParser\Parser\Php7($lexer);
+$parser = new Php7($lexer);
 
-$visitor = new class extends PhpParser\NodeVisitorAbstract {
+$visitor = new class extends NodeVisitorAbstract {
     public $path = null;
     public $code = null;
 
-    public function enterNode(PhpParser\Node $node) {
+    public function enterNode(Node $node) {
         foreach ($node->getComments() as $comment) {
             if (substr($comment->getText(), 0, 2) === '#[') {
                 echo $this->path . "\n";
@@ -29,15 +38,15 @@ $visitor = new class extends PhpParser\NodeVisitorAbstract {
     }
 };
 
-$traverser = new PhpParser\NodeTraverser;
+$traverser = new NodeTraverser;
 $traverser->addVisitor($visitor);
 $analyzer = new Analyzer();
 $files = $analyzer->getPhpFiles(__DIR__ . '/extracted');
 
-$i = 0;
+$index = 0;
 foreach ($files as $path) {
-    if (++$i % 1000 === 0) {
-        echo $i . "\n";
+    if (++$index % 1000 === 0) {
+        echo $index . "\n";
     }
 
     $code = file_get_contents($path);
@@ -47,8 +56,8 @@ foreach ($files as $path) {
         $visitor->path = $path;
         $visitor->code = $code;
         $traverser->traverse($stmts);
-    } catch (PhpParser\Error $e) {
-        echo $path . "\n";
-        echo "Parse error: " . $e->getMessage() . "\n";
+    } catch (ParserError $exception) {
+        error_log($path);
+        error_log("Parse error: " . $exception->getMessage());
     }
 }
